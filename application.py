32425bbb8098b77e6075238c8b5769658d3b98ad
gusdntj <gusdntj@gmail.com>
2023-04-27 07:59:52 +0000

    message

---
+++ application.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import sys
+application = Flask(__name__)
+
+
+@application.route("/")
+def hello():
+    return render_template("hello.html")
+
+@application.route("/apply")
+def apply():
+    return render_template("apply.html")
+
+@application.route("/list")
+def list():
+    return render_template("list.html")
+
+
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
